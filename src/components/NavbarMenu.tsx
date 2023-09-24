6aad70084136643c3bc745b2efa9de6c97a8afc6
emxanuel <ealg0528@gmail.com>
2023-09-23 23:14:30 -0400

    nav menu display changed to fixed

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const NavbarMenu = () => {
     return (
-        <ul className="flex -mt-96 duration-300 flex-col absolute w-full text-white z-10 bgBlue items-center" id='menu'>
+        <ul className="flex -mt-96 duration-300 flex-col fixed w-full text-white z-10 bgBlue items-center" id='menu'>
             <li className="duration-300 hover:scale-110 "><Link to="/">Inicio</Link></li>
             <li className="duration-300 hover:scale-110 "><Link to="/quienes-somos">Quienes Somos</Link></li>
             <li className="duration-300 hover:scale-110 "><Link to="/sucursales">Sucursales</Link></li>
