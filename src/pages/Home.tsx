a8818b3c229a670c91d7417f5a53ab4292f40238
emxanuel <ealg0528@gmail.com>
2023-09-23 22:17:10 -0400

    video added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Slide from "../components/Slide"
 import img1 from '../assets/CINTILLO (1).webp'
 import img2 from '../assets/SERVICIOS (1).webp'
 import img3 from '../assets/SUCURSALES (1).webp'
+import video from '../assets/VIDEO.mp4'
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <img className="w-full" src={img3} alt="" />
             <img className="w-full" src={img2} alt="" />
             <img className="w-full" src={img1} alt="" />
+            <video src={video} autoPlay loop muted controls className="w-full" />
         </div>
     )
 }
