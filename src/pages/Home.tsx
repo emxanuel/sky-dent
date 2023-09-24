fdf969068f57d9cf63c5119ed35d0c66be7c6156
emxanuel <ealg0528@gmail.com>
2023-09-23 20:38:03 -0400

    images changed to webp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import Slide from "../components/Slide"
-import img1 from '../assets/CINTILLO (1).png'
-import img2 from '../assets/SERVICIOS (1).png'
-import img3 from '../assets/SUCURSALES (1).png'
+import img1 from '../assets/CINTILLO (1).webp'
+import img2 from '../assets/SERVICIOS (1).webp'
+import img3 from '../assets/SUCURSALES (1).webp'
 
 const Home = () => {
     return (
