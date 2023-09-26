e4388d15ba10807bdd2aa65a04c7e94ccf7076c2
emxanuel <ealg0528@gmail.com>
2023-09-25 22:38:01 -0400

    fade added to offices and padding in offices removed in mobile

--- src/components/Offices.tsx
+++ src/components/Offices.tsx
@@ -1,20 +1,23 @@
 import { offices } from "../data/offices"
 import pin from '../assets/location-pin.webp'
+import { Fade } from 'react-awesome-reveal'
 
-interface Props{
+interface Props {
     gap?: string,
     footer?: boolean
 }
 
-const Offices: React.FC<Props> = ({gap, footer}) => {
+const Offices: React.FC<Props> = ({ gap, footer }) => {
     return (
-        <div className={`flex md:flex-row gap-10 md:gap-0 md:h-32 justify-evenly my-4 items-center ${gap? gap : ''} ${footer? 'flex-row w-full' : 'flex-col'}`}>
+        <div className={`flex md:flex-row gap-10 md:gap-0 md:h-32 justify-evenly my-4 items-center ${gap ? gap : ''} ${footer ? 'flex-row w-full' : 'flex-col'}`}>
             {offices.map((o) => (
-                <div key={o.id} className="flex flex-col items-center">
-                    <img className={`select-none w-12 md:w-16 ${footer? 'w-12 md:w-16' : ''}`} src={pin} alt="" />
-                    <h2 className={`textBlue font-bold text-2xl md:text-3xl ${footer? 'text-xs md:text-3xl' : ''}`}>{o.location}</h2>
-                    <p className={`textBlue text-xl md:text-2xl ${footer? 'text-xs md:text-2xl' : ''}`}>{o.phone}</p>
-                </div>
+                <Fade triggerOnce>
+                    <div key={o.id} className="flex flex-col items-center">
+                        <img className={`select-none w-12 md:w-16 ${footer ? 'w-12 md:w-16' : ''}`} src={pin} alt="" />
+                        <h2 className={`textBlue font-bold text-2xl md:text-3xl ${footer ? 'text-xs md:text-3xl' : ''}`}>{o.location}</h2>
+                        <p className={`textBlue text-xl md:text-2xl ${footer ? 'text-xs md:text-2xl' : ''}`}>{o.phone}</p>
+                    </div>
+                </Fade>
             ))}
         </div>
     )
