e1277668b4af1f436077bee659245908b6ca4aa3
emxanuel <ealg0528@gmail.com>
2023-10-10 17:59:24 -0400

    menu closed on click

--- src/components/NavbarMenu.tsx
+++ src/components/NavbarMenu.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
 import { scrollElement, scrollZero } from '../functions/elements'
 import { useNavigate } from 'react-router-dom'
+import { toggleShowElement } from '../functions/elements'
 
 const NavbarMenu = () => {
     const navigate = useNavigate()
     return (
-        <ul className="flex -mt-96 duration-300 flex-col fixed w-full text-white z-10 bgBlue items-center" id='menu'>
+        <ul className="flex -mt-96 duration-300 flex-col fixed w-full text-white z-10 bgBlue items-center" id='menu' onClick={() => {
+            const element = document.getElementById('menu') as HTMLElement
+            toggleShowElement(element)
+        }}>
             <li className="duration-300 hover:scale-110 "><button onClick={() => scrollElement('home', navigate)}>Inicio</button></li>
             <li className="duration-300 hover:scale-110 "><button onClick={() => scrollElement('about', navigate)}>Quienes Somos</button></li>
             <li className="duration-300 hover:scale-110 "><button onClick={() => scrollElement('blueLife', navigate)}>Resp. Soc.</button></li>
