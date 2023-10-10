c33f15ed95eec77aff1040f2c3b0d797354a7a00
emxanuel <ealg0528@gmail.com>
2023-10-10 19:13:21 -0400

    sky cash text in same color

--- src/components/Financing.tsx
+++ src/components/Financing.tsx
@@ -6,7 +6,7 @@ const Financing = () => {
                 <span>Disfruta de las atractivas facilidades con nuestros financiamientos</span>
                 <span className="flex items-center gap-1 justify-center h-full">
                     <span className="font-extrabold text-neutral-500 text-5xl">SKY</span>
-                    <span className="font-benedict font-normal text-black text-5xl h-8">CASH</span>
+                    <span className="font-benedict font-normal text-5xl h-8 text-neutral-500">CASH</span>
                 </span>
             </p>
             <a href="https://wa.me/8496338938" target="_blank" className="bgBlue text-white uppercase px-2 py-1 rounded-full text-base hover:scale-105 duration-200">
