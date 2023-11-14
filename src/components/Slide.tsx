1a52f4d94a57f964c11d2234b9eeb5f06cd2dbbe
emxanuel <ealg0528@gmail.com>
2023-11-13 20:16:14 -0400

    remove benefits

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -54,16 +54,6 @@ const Slide = () => {
                     </SwiperSlide>
                 ))}
             </Swiper>
-            <div className='w-full h-full z-10 md:bg-black md:absolute md:bg-opacity-40 md:text-white flex flex-col p-10 border-b-2 border-gray-700'>
-                <h2 className='md:text-6xl text-3xl '>Sky Dent</h2>
-                <h3 className='md:text-3xl md:mt-10 text-lg font-semibold mt-5'>Beneficios de atenderse con nosotros:</h3>
-                <ul className='md:text-2xl md:ml-6 md:mt-5 mt-4 ml-3'>
-                    <li>Beneficio 1</li>
-                    <li>Beneficio 2</li>
-                    <li>Beneficio 3</li>
-                </ul>
-                <button className='bg-[#0c4e9c] text-white mt-10 py-[5px] w-fit self-center px-4 rounded-md hover:scale-110 text-lg duration-200 md:bottom-5 md:absolute md:text-xl' onClick={() => scrollElement('services', navigate)}>Nuestros servicios</button>
-            </div>
         </div>
     )
 }
