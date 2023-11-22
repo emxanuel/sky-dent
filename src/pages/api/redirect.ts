96bbc940fb4404041a4c7a519d89b8c898c67533
emxanuel <ealg0528@gmail.com>
2023-11-21 23:52:24 -0400

    added vercel adapter

--- src/pages/api/redirect.ts
+++ src/pages/api/redirect.ts
@@ -1,8 +1,8 @@
+export const prerender = false
+
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
     const data = await request.body;
     return new Response(data, { status: 200 });
 };
-
-export const prerender = false
